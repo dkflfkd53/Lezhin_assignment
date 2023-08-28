331521323305b0804afc3a1b51bbf2e4f9af5592
user <b57062562@gmail.com>
2023-08-29 07:41:32 +0900

    ♻️ :: 어노테이션 추가

--- src/main/java/com/example/lezhinassignment/LezhinAssignmentApplication.java
+++ src/main/java/com/example/lezhinassignment/LezhinAssignmentApplication.java
@@ -2,8 +2,11 @@ package com.example.lezhinassignment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class LezhinAssignmentApplication {
 
     public static void main(String[] args) {
