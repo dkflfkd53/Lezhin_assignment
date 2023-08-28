5bde881fdd9478270249912f47346c5d614b7820
user <b57062562@gmail.com>
2023-08-28 18:32:30 +0900

    🌧 :: config 설정

--- src/main/java/com/example/lezhinassignment/global/config/SecurityConfig.java
+++ src/main/java/com/example/lezhinassignment/global/config/SecurityConfig.java
@@ -1,2 +1,55 @@
-package com.example.lezhinassignment.global.config;public class SecurityConfig {
+package com.example.lezhinassignment.global.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
+    private final ObjectMapper objectMapper;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+
+        return httpSecurity
+                .csrf().disable()
+                .cors().and()
+                .exceptionHandling()
+
+                .and()
+                .headers()
+                .frameOptions()
+                .sameOrigin()
+
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
+                .and()
+                .authorizeRequests()
+                .antMatchers("/login", "/signup").permitAll()
+                .antMatchers("/user", "/work").authenticated()
+                .anyRequest().permitAll()
+
+                .and()
+                .apply(new FilterConfig(jwtTokenProvider, objectMapper))
+                .and()
+                .build();
+
+    }
+
 }
