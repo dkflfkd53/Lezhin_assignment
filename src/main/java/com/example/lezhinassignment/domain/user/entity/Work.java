7362fa88707c5362c5fa1793ac4aada91f1ade23
user <b57062562@gmail.com>
2023-08-28 13:44:27 +0900

    🌧 :: 작품 entity 추가

---
+++ src/main/java/com/example/lezhinassignment/domain/user/entity/Work.java
@@ -0,0 +1,20 @@
+package com.example.lezhinassignment.domain.user.entity;
+
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+public class Work {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+}
