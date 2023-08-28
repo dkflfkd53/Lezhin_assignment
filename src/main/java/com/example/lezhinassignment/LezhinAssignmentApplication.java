ac96d88fbfa651c4571b397af4fd1f2ff911a028
user <b57062562@gmail.com>
2023-08-28 09:01:59 +0900

    first commit

---
+++ src/main/java/com/example/lezhinassignment/LezhinAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.lezhinassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LezhinAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LezhinAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lezhinassignment/LezhinAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lezhinassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LezhinAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
