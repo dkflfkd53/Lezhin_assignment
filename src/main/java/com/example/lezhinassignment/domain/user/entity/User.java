47b427c9d9184eae2d31bf90ab1cb1ae4cee392b
user <b57062562@gmail.com>
2023-08-28 10:40:17 +0900

    first commit

--- src/main/java/com/example/lezhinassignment/domain/user/entity/User.java
+++ src/main/java/com/example/lezhinassignment/domain/user/entity/User.java
@@ -1,16 +1,32 @@
 package com.example.lezhinassignment.domain.user.entity;
 
+import lombok.*;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.time.LocalDateTime;
 
 @Entity
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private
+    private String userName;
+
+    private String userEmail;
+
+    private String gender;
+
+    private String type;
+
+    private LocalDateTime registerDate;
+
 }
