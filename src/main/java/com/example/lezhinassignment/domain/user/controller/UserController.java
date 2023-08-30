79b0cbae978c13669bb496639d9f50721456c360
JungHyun <b57062562@gmail.com>
2023-08-30 20:21:20 +0900

    🌧 :: Login 로직 추가

--- src/main/java/com/example/lezhinassignment/domain/user/controller/UserController.java
+++ src/main/java/com/example/lezhinassignment/domain/user/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.example.lezhinassignment.domain.user.controller;
 
+import com.example.lezhinassignment.domain.user.dto.request.LoginRequest;
 import com.example.lezhinassignment.domain.user.dto.request.SignupRequest;
+import com.example.lezhinassignment.domain.user.dto.response.TokenResponse;
+import com.example.lezhinassignment.domain.user.service.UserLoginService;
 import com.example.lezhinassignment.domain.user.service.UserSignupService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,10 +14,17 @@ import javax.validation.Valid;
 @RestController
 @RequiredArgsConstructor
 public class UserController {
-    private final UserSignupService userLoginService;
+    private final UserSignupService userSignupService;
+    private final UserLoginService userLoginService;
 
     @PostMapping("/signup")
     public void signup(@Valid SignupRequest request) {
-        userLoginService.signup(request);
+        userSignupService.signup(request);
     }
+
+    @PostMapping("/login")
+    public TokenResponse login(@Valid LoginRequest request) {
+        return userLoginService.login(request);
+    }
+
 }
---
+++ src/main/java/com/example/lezhinassignment/domain/user/service/UserLoginService.java
@@ -0,0 +1,45 @@
+package com.example.lezhinassignment.domain.user.service;
+
+import com.example.lezhinassignment.domain.user.dto.request.LoginRequest;
+import com.example.lezhinassignment.domain.user.dto.response.TokenResponse;
+import com.example.lezhinassignment.domain.user.entity.User;
+import com.example.lezhinassignment.domain.user.repository.UserRepository;
+import com.example.lezhinassignment.global.exception.user.PasswordMismatchException;
+import com.example.lezhinassignment.global.exception.user.UserNotFoundException;
+import com.example.lezhinassignment.global.security.jwt.JwtProperties;
+import com.example.lezhinassignment.global.security.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.validation.Valid;
+import java.util.Date;
+
+@Service
+@RequiredArgsConstructor
+public class UserLoginService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+    private final JwtProperties jwtProperties;
+
+    public TokenResponse login(@Valid LoginRequest request) {
+
+        Date now = new Date();
+
+        User user = userRepository.findByUserName(request.getUserName())
+                .orElseThrow(()-> UserNotFoundException.EXCEPTION);
+
+        if(!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw PasswordMismatchException.EXCEPTION;
+        }
+
+        return TokenResponse.builder()
+                .accessToken(jwtTokenProvider.createAccessToken(user.getUserName()))
+                .accessExpiredAt(new Date(now.getTime() + jwtProperties.getAccessExpiration()))
+                .build();
+
+    }
+
+}
