ab04bdf0b9fbcd187dd3f1dfed40c2f5781ae2a7
user <b57062562@gmail.com>
2023-09-03 15:41:23 +0900

    🌧 :: 댓글 dto 추가

---
+++ src/main/java/com/example/lezhinassignment/domain/work/presentation/dto/request/WriteCommentRequest.java
@@ -0,0 +1,11 @@
+package com.example.lezhinassignment.domain.work.presentation.dto.request;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class WriteCommentRequest {
+    private String content;
+}
