e1cf1d8cb9fd3cf2ab3423b7d3ef6ed3aa6d3664
user <b57062562@gmail.com>
2023-09-03 15:40:18 +0900

    🌧 :: 댓글 객체 추가

---
+++ src/main/java/com/example/lezhinassignment/domain/work/entity/Comment.java
@@ -0,0 +1,29 @@
+package com.example.lezhinassignment.domain.work.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long userId;
+
+    private Long workId;
+
+    private String userName;
+
+    private String content;
+
+}
