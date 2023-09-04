0a7b619f4ce4b3e98a3fa824e447f025858e20b1
user <b57062562@gmail.com>
2023-09-04 22:46:00 +0900

    ♻️ :: WorkResponse 수정

--- src/main/java/com/example/lezhinassignment/domain/work/presentation/dto/response/WorkResponse.java
+++ src/main/java/com/example/lezhinassignment/domain/work/presentation/dto/response/WorkResponse.java
@@ -8,9 +8,11 @@ import lombok.Getter;
 @AllArgsConstructor
 public class WorkResponse {
 
+    private Long id;
     private String title;
 
     public WorkResponse(Work work) {
+        id = work.getId();
         title = work.getTitle();
     }
 
