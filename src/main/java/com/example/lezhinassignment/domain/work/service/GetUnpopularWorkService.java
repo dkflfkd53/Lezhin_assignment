38c76cf2f3f98dedcd51261975658f58ae6a7eac
user <b57062562@gmail.com>
2023-09-04 22:45:49 +0900

    ♻️ :: 비인기 작품 조회 기능 Querydsl 적용

--- src/main/java/com/example/lezhinassignment/domain/work/service/GetUnpopularWorkService.java
+++ src/main/java/com/example/lezhinassignment/domain/work/service/GetUnpopularWorkService.java
@@ -1,40 +1,37 @@
 package com.example.lezhinassignment.domain.work.service;
 
-import com.example.lezhinassignment.domain.work.entity.DisLike;
+import com.example.lezhinassignment.domain.work.entity.QDisLike;
+import com.example.lezhinassignment.domain.work.entity.QWork;
 import com.example.lezhinassignment.domain.work.entity.Work;
-import com.example.lezhinassignment.domain.work.repository.DisLikeRepository;
-import com.example.lezhinassignment.domain.work.repository.WorkRepository;
+import com.example.lezhinassignment.domain.work.presentation.dto.response.WorkResponse;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class GetUnpopularWorkService {
 
-    private final WorkRepository workRepository;
-    private final DisLikeRepository disLikeRepository;
+    private final JPAQueryFactory queryFactory;
 
-    public List<Work> getUnpopularWork() {
-        List<Long> workIds = disLikeRepository.findAll()
-                .stream()
-                .collect(Collectors.groupingBy(DisLike::getWorkId, Collectors.counting()))
-                .entrySet()
-                .stream()
-                .sorted(Map.Entry.<Long, Long>comparingByValue().reversed())
+    public List<WorkResponse> getUnpopularWork() {
+        QWork work = new QWork("works");
+        QDisLike disLike = new QDisLike("disLikes");
+
+        List<Work> works =  queryFactory
+                .selectFrom(work)
+                .leftJoin(work.disLikes, disLike)
+                .groupBy(work)
+                .orderBy(disLike.count().desc())
                 .limit(3)
-                .map(Map.Entry::getKey)
-                .collect(Collectors.toList());
+                .fetch();
 
-        return workIds
+        return works
                 .stream()
-                .map(workRepository::findById)
-                .filter(Optional::isPresent)
-                .map(Optional::get)
+                .map(WorkResponse::new)
                 .collect(Collectors.toList());
     }
 
