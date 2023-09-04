cc59f32bc0aac7168a2bb54a72d8aaafe2d39cbf
user <b57062562@gmail.com>
2023-09-04 22:44:00 +0900

    🐛:: 좋아요 오류 해결

--- src/main/java/com/example/lezhinassignment/domain/work/service/AddLikeService.java
+++ src/main/java/com/example/lezhinassignment/domain/work/service/AddLikeService.java
@@ -2,7 +2,9 @@ package com.example.lezhinassignment.domain.work.service;
 
 import com.example.lezhinassignment.domain.user.entity.User;
 import com.example.lezhinassignment.domain.user.service.facade.UserFacade;
+import com.example.lezhinassignment.domain.work.entity.DisLike;
 import com.example.lezhinassignment.domain.work.entity.Like;
+import com.example.lezhinassignment.domain.work.entity.Work;
 import com.example.lezhinassignment.domain.work.repository.DisLikeRepository;
 import com.example.lezhinassignment.domain.work.repository.LikeRepository;
 import com.example.lezhinassignment.domain.work.repository.WorkRepository;
@@ -11,6 +13,8 @@ import com.example.lezhinassignment.global.exception.work.WorkNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -25,21 +29,28 @@ public class AddLikeService {
     public void addLike(Long workId) {
         User user = userFacade.currentUser();
 
-        if(!workRepository.existsById(workId)) {
-            throw WorkNotFoundException.EXCEPTION;
-        }
+        Work work = workRepository.findById(workId)
+                .orElseThrow(()->WorkNotFoundException.EXCEPTION);
 
         if(likeRepository.findByUserIdAndWorkId(user.getId(), workId).isPresent()) {
             throw AlreadyLikeException.EXCEPTION;
         }
 
-        disLikeRepository.deleteByWorkId(workId);
+        Optional<DisLike> disLikeOptional = disLikeRepository.findByUserIdAndWorkId(user.getId(), workId);
 
-        likeRepository.save(
+        if(disLikeOptional.isPresent()) {
+            DisLike disLike = disLikeOptional.get();
+            work.removeDisLike(disLike);
+            disLikeRepository.delete(disLike);
+        }
+
+        Like like = likeRepository.save(
                 Like.builder()
                         .userId(user.getId())
                         .workId(workId)
                         .build());
+
+        work.addLike(like);
     }
 
 }
