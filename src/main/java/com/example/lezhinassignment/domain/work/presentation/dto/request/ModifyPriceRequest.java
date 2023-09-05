211ca2a29d2ba0e6dc754c75c030f6337f5f9bef
JungHyun <b57062562@gmail.com>
2023-09-05 11:40:46 +0900

    🐛:: @Transactional 추가

--- src/main/java/com/example/lezhinassignment/domain/work/presentation/dto/request/ModifyPriceRequest.java
+++ src/main/java/com/example/lezhinassignment/domain/work/presentation/dto/request/ModifyPriceRequest.java
@@ -7,6 +7,5 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor(access =  AccessLevel.PROTECTED)
 public class ModifyPriceRequest {
-
     private Long price;
 }
--- src/main/java/com/example/lezhinassignment/domain/work/service/ModifyPriceService.java
+++ src/main/java/com/example/lezhinassignment/domain/work/service/ModifyPriceService.java
@@ -6,9 +6,11 @@ import com.example.lezhinassignment.domain.work.repository.WorkRepository;
 import com.example.lezhinassignment.global.exception.work.WorkNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class ModifyPriceService {
 
     private final WorkRepository workRepository;
