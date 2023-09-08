c3357d0edb2adb3b1fa40a3b6ab58b0bb53e2a9e
user <b57062562@gmail.com>
2023-09-08 14:17:37 +0900

    🌧 :: user로 작품 삭제 기능 추가

--- src/main/java/com/example/lezhinassignment/domain/work/repository/WorkRepository.java
+++ src/main/java/com/example/lezhinassignment/domain/work/repository/WorkRepository.java
@@ -1,9 +1,9 @@
 package com.example.lezhinassignment.domain.work.repository;
 
+import com.example.lezhinassignment.domain.user.entity.User;
 import com.example.lezhinassignment.domain.work.entity.Work;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface WorkRepository extends JpaRepository<Work, Long>, CustomWorkRepository {
-
-
+    void deleteAllByUser(User user);
 }
