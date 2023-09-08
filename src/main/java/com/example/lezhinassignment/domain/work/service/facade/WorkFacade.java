a1f4bdbaf4567e7f30d66b4672340de7a5f89902
user <b57062562@gmail.com>
2023-09-08 09:52:38 +0900

    🌧 :: CurrentComment 기능 추가

--- src/main/java/com/example/lezhinassignment/domain/work/service/facade/WorkFacade.java
+++ src/main/java/com/example/lezhinassignment/domain/work/service/facade/WorkFacade.java
@@ -1,7 +1,10 @@
 package com.example.lezhinassignment.domain.work.service.facade;
 
+import com.example.lezhinassignment.domain.work.entity.Comment;
 import com.example.lezhinassignment.domain.work.entity.Work;
+import com.example.lezhinassignment.domain.work.repository.CommentRepository;
 import com.example.lezhinassignment.domain.work.repository.WorkRepository;
+import com.example.lezhinassignment.global.exception.work.CommentNotFoundException;
 import com.example.lezhinassignment.global.exception.work.WorkNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -11,9 +14,16 @@ import org.springframework.stereotype.Component;
 public class WorkFacade {
 
     private final WorkRepository workRepository;
+    private final CommentRepository commentRepository;
 
     public Work currentWork(Long workId) {
         return workRepository.findById(workId)
                 .orElseThrow(()-> WorkNotFoundException.EXCEPTION);
     }
+
+    public Comment currentComment(Long userId, Long workId) {
+        return commentRepository.findByUserIdAndWorkId(userId, workId)
+                .orElseThrow(()-> CommentNotFoundException.EXCEPTION);
+    }
+
 }
