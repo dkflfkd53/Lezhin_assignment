f56385330dba618a24e4ab9231e193b32160095f
user <b57062562@gmail.com>
2023-09-08 14:15:45 +0900

    ♻️ :: getAdult 코드 짜보기

--- src/main/java/com/example/lezhinassignment/domain/user/repository/CustomUserRepository.java
+++ src/main/java/com/example/lezhinassignment/domain/user/repository/CustomUserRepository.java
@@ -1,9 +1,9 @@
 package com.example.lezhinassignment.domain.user.repository;
 
-import com.example.lezhinassignment.domain.user.entity.User;
+import com.example.lezhinassignment.domain.work.entity.Visit;
 
 import java.util.List;
 
 public interface CustomUserRepository {
-    List<User> getAdultUser();
+    List<Visit> getAdultUser();
 }
--- src/main/java/com/example/lezhinassignment/domain/user/repository/CustomUserRepositoryImpl.java
+++ src/main/java/com/example/lezhinassignment/domain/user/repository/CustomUserRepositoryImpl.java
@@ -1,6 +1,7 @@
 package com.example.lezhinassignment.domain.user.repository;
 
-import com.example.lezhinassignment.domain.user.entity.User;
+import com.example.lezhinassignment.domain.work.entity.Visit;
+import com.example.lezhinassignment.domain.work.enums.WorkType;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 import static com.example.lezhinassignment.domain.user.entity.QUser.user;
+import static com.example.lezhinassignment.domain.work.entity.QVisit.visit;
 import static com.example.lezhinassignment.domain.work.entity.QWork.work;
 
 @RequiredArgsConstructor
@@ -16,10 +18,18 @@ public class CustomUserRepositoryImpl implements CustomUserRepository {
 
     private final JPAQueryFactory queryFactory;
 
-    public List<User> getAdultUser() {
+    public List<Visit> getAdultUser() {
+//        return queryFactory
+//                .selectFrom(user)
+//                .join(user.AdultWork, work)
+//                .groupBy(user)
+//                .having(work.id.countDistinct().goe(3))
+//                .fetch();
+
         return queryFactory
-                .selectFrom(user)
-                .join(user.AdultWork, work)
+                .selectFrom(visit)
+                .join(visit.work)
+                .where(work.workType.eq(WorkType.ADULT))
                 .groupBy(user)
                 .having(work.id.countDistinct().goe(3))
                 .fetch();
