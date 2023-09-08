78749ac2c08fd1f624046ea067f5658a010c6808
user <b57062562@gmail.com>
2023-09-08 12:23:44 +0900

    ♻️ :: visit 테이블 양방향 매핑

--- src/main/java/com/example/lezhinassignment/domain/work/service/work/GetWorkRecordService.java
+++ src/main/java/com/example/lezhinassignment/domain/work/service/work/GetWorkRecordService.java
@@ -1,7 +1,6 @@
 package com.example.lezhinassignment.domain.work.service.work;
 
 import com.example.lezhinassignment.domain.user.presentation.dto.response.UserResponse;
-import com.example.lezhinassignment.domain.user.repository.UserRepository;
 import com.example.lezhinassignment.domain.work.entity.Visit;
 import com.example.lezhinassignment.domain.work.repository.VisitRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,16 +14,11 @@ import java.util.stream.Collectors;
 public class GetWorkRecordService {
 
     private final VisitRepository visitRepository;
-    private final UserRepository userRepository;
 
     public List<UserResponse> getWorkRecord(Long workId) {
-        List<Long> ids = visitRepository.findAllByWorkId(workId)
+        return visitRepository.findAllByWorkId(workId)
                 .stream()
-                .map(Visit::getUserId)
-                .collect(Collectors.toList());
-
-        return userRepository.findAllById(ids)
-                .stream()
+                .map(Visit::getUser)
                 .map(UserResponse::new)
                 .collect(Collectors.toList());
     }
--- src/main/java/com/example/lezhinassignment/domain/work/service/work/GetWorkService.java
+++ src/main/java/com/example/lezhinassignment/domain/work/service/work/GetWorkService.java
@@ -4,7 +4,6 @@ import com.example.lezhinassignment.domain.user.entity.User;
 import com.example.lezhinassignment.domain.user.service.facade.UserFacade;
 import com.example.lezhinassignment.domain.work.entity.Visit;
 import com.example.lezhinassignment.domain.work.entity.Work;
-import com.example.lezhinassignment.domain.work.enums.WorkType;
 import com.example.lezhinassignment.domain.work.presentation.dto.response.WorkResponse;
 import com.example.lezhinassignment.domain.work.repository.VisitRepository;
 import com.example.lezhinassignment.domain.work.service.facade.WorkFacade;
@@ -28,14 +27,12 @@ public class GetWorkService {
 
         Work work = workFacade.currentWork(workId);
 
-        if(work.getWorkType() == WorkType.ADULT)user.addAdultWork(work);
-
         user.updateVisitTime(now);
 
         visitRepository.save(
                 Visit.builder()
-                        .workId(workId)
-                        .userId(user.getId())
+                        .user(user)
+                        .work(work)
                         .build());
 
         return new WorkResponse(work);
