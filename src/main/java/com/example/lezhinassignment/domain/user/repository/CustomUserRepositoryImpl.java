5a883f17f63e793e9d32ef0cc5961cbd3b68df54
user <b57062562@gmail.com>
2023-09-12 08:38:08 +0900

    ♻️ :: 성인 조회 유저 조회 기능 수정

--- src/main/java/com/example/lezhinassignment/domain/user/repository/CustomUserRepositoryImpl.java
+++ src/main/java/com/example/lezhinassignment/domain/user/repository/CustomUserRepositoryImpl.java
@@ -1,14 +1,12 @@
 package com.example.lezhinassignment.domain.user.repository;
 
 import com.example.lezhinassignment.domain.work.entity.Visit;
-import com.example.lezhinassignment.domain.work.enums.WorkType;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-import static com.example.lezhinassignment.domain.user.entity.QUser.user;
 import static com.example.lezhinassignment.domain.work.entity.QVisit.visit;
 import static com.example.lezhinassignment.domain.work.entity.QWork.work;
 
@@ -19,19 +17,11 @@ public class CustomUserRepositoryImpl implements CustomUserRepository {
     private final JPAQueryFactory queryFactory;
 
     public List<Visit> getAdultUser() {
-//        return queryFactory
-//                .selectFrom(user)
-//                .join(user.AdultWork, work)
-//                .groupBy(user)
-//                .having(work.id.countDistinct().goe(3))
-//                .fetch();
-
         return queryFactory
                 .selectFrom(visit)
-                .join(visit.work)
-                .where(work.workType.eq(WorkType.ADULT))
-                .groupBy(user)
-                .having(work.id.countDistinct().goe(3))
+                .join(visit.work, work)
+                .groupBy(visit)
+                .having(work.countDistinct().goe(3))
                 .fetch();
     }
 }
