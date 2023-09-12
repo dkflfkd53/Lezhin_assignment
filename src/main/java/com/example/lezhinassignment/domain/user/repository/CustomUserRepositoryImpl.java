433fee0bf5334f327689fa1bc05f8ca346c4336f
user <b57062562@gmail.com>
2023-09-12 21:49:44 +0900

    ♻️ :: querydsl 코드 수정

--- src/main/java/com/example/lezhinassignment/domain/user/repository/CustomUserRepositoryImpl.java
+++ src/main/java/com/example/lezhinassignment/domain/user/repository/CustomUserRepositoryImpl.java
@@ -1,12 +1,14 @@
 package com.example.lezhinassignment.domain.user.repository;
 
-import com.example.lezhinassignment.domain.work.entity.Visit;
+import com.example.lezhinassignment.domain.user.entity.User;
+import com.example.lezhinassignment.domain.work.enums.WorkType;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+import static com.example.lezhinassignment.domain.user.entity.QUser.user;
 import static com.example.lezhinassignment.domain.work.entity.QVisit.visit;
 import static com.example.lezhinassignment.domain.work.entity.QWork.work;
 
@@ -16,12 +18,22 @@ public class CustomUserRepositoryImpl implements CustomUserRepository {
 
     private final JPAQueryFactory queryFactory;
 
-    public List<Visit> getAdultUser() {
-        return queryFactory
-                .selectFrom(visit)
-                .join(visit.work, work)
-                .groupBy(visit)
-                .having(work.countDistinct().goe(3))
+    public List<User> getAdultUser() {
+        /*return queryFactory.select(user)
+                .join(visit).on(user.id.eq(visit.user.id))
+                .join(work).on(visit.work.id.eq(work.id).and(work.workType.eq(WorkType.ADULT)))
+                .groupBy(user)
+                .having(visit.count().gt(3))
+                .fetch();*/
+
+        return queryFactory.selectFrom(user)
+                .join(user.visits, visit)
+                .join(visit.work, work).on(work.workType.eq(WorkType.ADULT))
+                .groupBy(user.id, work.id)
+                .having(work.count().gt(3))
                 .fetch();
     }
 }
+
+
+
